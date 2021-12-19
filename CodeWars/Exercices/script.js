62e5c9a549e489b853b6cf9cb2a394a494a6800d
Bluexorcist <xblueexorcistx@gmail.com>
2021-12-19 10:33:53 +0200

    Seventh commit

---
+++ CodeWars/Exercices/script.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+
+function squareSumsRow(N) {
+    
+    let arr = [];
+    for(let i = 1; i <= N; i++) {
+        arr.push(i);
+    }
+    console.log(arr);
+};
+squareSumsRow(15);
